ebb89064e339d76e0d2c815fb203f99a60aa26d5
Cerbiasz <blazejzajaczkowski@gmail.com>
2026-03-01 21:58:08 +0100

    poprawiamy

--- kalkulator.py
+++ kalkulator.py
@@ -1,14 +1,16 @@
-  def dodaj(a, b):
-      return a + b
+def dodaj(a, b):
+    return a + b
 
-  def odejmij(a, b):
-      return a - b
 
-  def pomnoz(a, b):
-      return a * b
+def odejmij(a, b):
+    return a - b
 
-  def podziel(a, b):
-      if b == 0:
-          raise ValueError("Nie można dzielić przez zero!")
-      return a / b
-  EOF
+
+def pomnoz(a, b):
+    return a * b
+
+
+def podziel(a, b):
+    if b == 0:
+        raise ValueError("Nie mozna dzielic przez zero!")
+    return a / b
